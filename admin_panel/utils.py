741e4a784bc0c83dbb716d8b0b57554185489d41
festouchnigeria <festouchnigerialimited@gmail.com>
2024-08-03 13:55:58 +0100

    final commit

--- admin_panel/utils.py
+++ admin_panel/utils.py
@@ -254,7 +254,7 @@ class Func:
                 'bank_code': loan.user.disbursementaccount.bank_code,
                 'account_number': loan.user.disbursementaccount.number,
                 # 'amount': loan.principal_amount - ((loan.interest_perc / 100) * loan.principal_amount),
-                'amount': 100,
+                'amount': 2000,
                 'currency': 'NGN',
                 'narration': 'MG Loan',
                 'reference': f'{loan.loan_id}-{admin_user.id}',
