b435e624078e9e43d16b738b8a2691aaa6e4e46b
festouchnigeria <festouchnigerialimited@gmail.com>
2024-08-04 03:17:22 +0100

    final commit

--- admin_panel/utils.py
+++ admin_panel/utils.py
@@ -264,7 +264,7 @@ class Func:
                 'amount': loan.principal_amount - ((loan.interest_perc / 100) * loan.principal_amount),
                 'currency': 'NGN',
                 'narration': 'MG Loan',
-                'reference': f'{loan.loan_id}-{admin_user.id}',
+                'reference': f'{loan.loan_id}-{admin_user.id}-{random.randint(100, 999)}',
                 'meta': [
                     {
                         'email': loan.user.email,
